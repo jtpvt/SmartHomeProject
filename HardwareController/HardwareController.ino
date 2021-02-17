595fda7198e32b65a327f8201cc6f5859dfa139e
Justin <justintpotter@vt.edu>
2021-02-17 14:13:47 -0500

    Add serial voltage reading definitions
    5V -> 1024
    0V -> 0

--- HardwareController/HardwareController.ino
+++ HardwareController/HardwareController.ino
@@ -3,6 +3,10 @@
  * Arduino hardware controller for smart home project
  */
 
+// general definitions
+const int V_MAX = 1024,  // +5V
+          V_MIN =    0;  //  0V
+          
 void setup()
 {
   Serial.begin(9600);
