8802199dc1bd57aa41c1c305f6f882b89f5483b5
Justin <justintpotter@vt.edu>
2021-02-27 02:20:33 -0500

    Initialize WeatherStation class

--- HardwareController/HardwareController.ino
+++ HardwareController/HardwareController.ino
@@ -3,6 +3,8 @@
  * Arduino hardware controller for smart home project
  */
 
+#include "WeatherStation.h"
+
 // general definitions
 const int V_MAX = 1024,  // +5V
           V_MIN =    0;  //  0V
@@ -10,12 +12,11 @@ const int V_MAX = 1024,  // +5V
 void setup()
 {
   Serial.begin(9600);
+
+  
 }
 
 void loop()
 {
-  if (Serial.available())
-  {
-    char val = Serial.read();
-  }
+  
 }
---
+++ HardwareController/WeatherStation.h
@@ -0,0 +1,10 @@
+// Weather Station
+/*
+ * The weather station reads room temperature and humidity to determine whether to
+ * activate or deactivate air conditioner or heater.
+ */
+
+class WeatherStation
+{
+
+};
