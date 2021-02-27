629f739a23dcfac3c7a94a7fc08a163bfc439ef2
Justin <justintpotter@vt.edu>
2021-02-27 02:21:44 -0500

    Declare and initialize WeatherStation variables

--- HardwareController/WeatherStation.h
+++ HardwareController/WeatherStation.h
@@ -6,5 +6,10 @@
 
 class WeatherStation
 {
-
+  private:
+    const int DIVIDE_R = 270,   // resistor for voltage divider (Ohms)
+              SOURCE_V = 1024;  // max 10-bit int = 5V
+    float temperatureC,  // temperature (degC)
+          humidity;      // relative humidity (%RH)
+    int thermistorZ, humiditySensorZ;  // impedances of components (kOhms)
 };
