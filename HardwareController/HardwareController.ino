60f533703f07b1184a364a16729301f876b7612b
Justin <justintpotter@vt.edu>
2021-02-28 17:31:34 -0500

    Instantiate IntruderAlert in main file

--- HardwareController/HardwareController.ino
+++ HardwareController/HardwareController.ino
@@ -12,6 +12,7 @@ const int V_MAX = 1024,  // +5V
           V_MIN =    0;  //  0V
 
 // object instantiations for smart home features
+IntruderAlert  IA;
 NightLight     NL;
 WeatherStation WS;
           
@@ -19,6 +20,7 @@ void setup()
 {
   Serial.begin(9600);
 
+  IA.init();
   NL.init();
   WS.init();
 }
