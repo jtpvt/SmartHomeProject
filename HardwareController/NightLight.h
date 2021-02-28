15967533d767fef816d474046748735eb30dc454
Justin <justintpotter@vt.edu>
2021-02-27 19:33:11 -0500

    Initialize NightLight class

---
+++ HardwareController/NightLight.h
@@ -0,0 +1,10 @@
+// Night Light
+/*
+ * The night light reads ambient brightness in a room to determine whether to activate or deactivate an LED.
+ */
+
+ class NightLight
+ {
+  public:
+    void init() {}
+ };
