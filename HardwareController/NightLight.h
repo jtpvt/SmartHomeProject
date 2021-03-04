aaf805fe868eceeeb3088d7d16742c00c49cfaad
Justin <justintpotter@vt.edu>
2021-03-03 23:26:48 -0500

    Create NightLight function that returns result of voltage divider as 10-bit int

--- HardwareController/NightLight.h
+++ HardwareController/NightLight.h
@@ -3,8 +3,16 @@
  * The night light reads ambient brightness in a room to determine whether to activate or deactivate an LED.
  */
 
+ #define ANALOG_IN A0
+
  class NightLight
  {
   public:
     void init() {}
+
+    // returns voltage divider result as 10-bit int (0 = 0V, 1024 = 5V)
+    int dividedVoltage()
+    {
+      return analogRead(A0);
+    }
  };
