5e8f920db960ccf34f914941153303fbfbf15866
Justin <justintpotter@vt.edu>
2021-03-07 18:37:36 -0500

    Create AudioAlert class

---
+++ HardwareController/AudioAlert.h
@@ -0,0 +1,14 @@
+// Audio Alert
+/*
+ * An amplifier and speaker play an audio alert on specific events
+ */
+ 
+class InfoDisplay
+{
+  public:
+    void init() {}
+    void call()
+    {
+      Serial.print("WARNING: AudioAlert call() not implemented!\n");
+    }
+};
