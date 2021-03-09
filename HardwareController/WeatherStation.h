2cce736b980c2b2c464536dd76f6467feeadf56b
Justin <justintpotter@vt.edu>
2021-03-08 22:56:00 -0500

    Change equation used to estimate temperature
    Before, the equation was a logarithmic regression of the data points given in the thermistor's datasheet. Now it is the proper equation given

--- HardwareController/WeatherStation.h
+++ HardwareController/WeatherStation.h
@@ -10,30 +10,34 @@ class WeatherStation
 {
   private:
     const int PIN_HEATER =  11,
-              PIN_AC     =  12,
-              DIVIDE_R   = 270;  // resistor for voltage divider (Ohms)
+              PIN_AC     =  12;  // resistor for voltage divider (Ohms)
+    // thermistor constants
+    const int B_CONSTANT          =  3936,  // Thermistor characteristic constant (Kelvin, K)
+              RESISTANCE_CONSTANT = 10000;  // Resistance at ambient temperature constant (Ohms)
+    const float TEMP_CONSTANT = 298.15;  // Ambient temperature constant, 25degC (Kelvin, K)
 
-    // Calculates and returns impedance (kOhms) with a 2-resistor voltage divider
-    int calculateComponentImpedance(int voltage)
+    // Calculates and returns impedance (Ohms) with a 2-resistor voltage divider
+    int calculateComponentResistance(float divideR, int voltageDrop)
     {
-      return DIVIDE_R / (1023 / voltage - 1); // component is R2
+      return divideR / (1023.0 / voltageDrop - 1.0); // component is R2
     }
 
     // Estimates humidity with logarithmic equation from a regression analysis of the dataset provided in the humidity sensor's datasheet
-    float estimateRelativeHumidity(float impedance)
+    float estimateRelativeHumidity(float resistance)
     {
-      float humiMin = -9.49422 * log(0.0000342857 * impedance);  // logarithmic regression at 15degC (59degF)
-      float humiMax = -10.1923 * log(0.0000895662 * impedance);  // logarithmic regression at 25degC (77degF)
+      float humiMin = -9.49422 * log(0.0000342857 * resistance);  // logarithmic regression at 15degC (59degF)
+      float humiMax = -10.1923 * log(0.0000895662 * resistance);  // logarithmic regression at 25degC (77degF)
 
+      // TODO: Replace current equations with new equations
       // TODO: Return estimate that's weighted based on the temperature. It's unlikely that temperature range will go outside a range of 15-25 degC (59-77 degF)
       
       return (humiMin + humiMax) / 2;
     }
 
-    // Estimates temperature with a logarithmic equation from regression analysis of the dataset provided in the thermistor's datasheet
-    float estimateTemperature(float impedance)
+    // Estimates and returns temperature (degC) with equation provided in thermistor's datasheet
+    float estimateTemperature(float resistance)
     {
-      return -24.2268 * log(0.339014 * impedance); // logarithmic regression
+      return 1.0/(log(resistance/RESISTANCE_CONSTANT)/B_CONSTANT + 1.0/TEMP_CONSTANT) - 273.15;  // temp_C = temp_K - 273.15
     }
 
 /*
@@ -46,10 +50,10 @@ class WeatherStation
     void call()
     {
       Serial.print("Humi: ");
-      Serial.print(estimateRelativeHumidity(calculateComponentImpedance(A0)));
+      Serial.print(estimateRelativeHumidity(calculateComponentResistance(10000, analogRead(A0))));
       Serial.print(" %RH\n");
       Serial.print("Temp: ");
-      Serial.print(estimateTemperature(calculateComponentImpedance(A1)));
+      Serial.print(estimateTemperature(calculateComponentResistance(10000, analogRead(A4))));
       Serial.print(" degC\n\n");
     }
 };
